68ebcc4021d85de8c43b28fa7a0912cdf3d85ed3
Richard <tuxbox.guru@gmail.com>
2021-11-07 18:15:35 +0000

    Silly mistake... new not allowed in Interrupt

--- esphome/components/pulse_meter/pulse_meter_sensor.cpp
+++ esphome/components/pulse_meter/pulse_meter_sensor.cpp
@@ -7,7 +7,7 @@ namespace pulse_meter {
 static const char *const TAG = "pulse_meter";
 
 void PulseMeterSensor::setup() {
-  this->pulse_q_ = xQueueCreate(10, sizeof(pulse_data_t *));
+  this->pulse_q_ = xQueueCreate(5, sizeof(pulse_data_t *));
   this->pin_->setup();
   this->isr_pin_ = pin_->to_isr();
   this->pin_->attach_interrupt(PulseMeterSensor::gpio_intr, this, gpio::INTERRUPT_ANY_EDGE);
@@ -18,14 +18,14 @@ void PulseMeterSensor::loop() {
     const uint32_t now = micros();
 
     uint32_t pulse_width;
-    pulse_data_t *pdata;
+    pulse_data_t pdata;
     if (xQueueReceive(this->pulse_q_, &pdata, 1L) == pdTRUE)
     {
      //ESP_LOGD(TAG, "Got pulse at %us",pdata->ts_value);
 
      if (last_pulse != 0)
       {
-         pulse_width = pdata->ts_value-last_pulse;
+         pulse_width = pdata.ts_value-last_pulse;
          if (pulse_width > this->filter_us_) 
            {
            ESP_LOGD(TAG, "A: Pulses/Min %f",(60.0 * 1000.0) / (pulse_width / 1000));
@@ -36,7 +36,7 @@ void PulseMeterSensor::loop() {
            }
   
 
-           this->last_pulse = pdata->ts_value;
+           this->last_pulse = pdata.ts_value;
            this->total_pulses_++;
            const uint32_t total = this->total_pulses_;
            if (this->total_dedupe_.next(total)) {
@@ -45,9 +45,9 @@ void PulseMeterSensor::loop() {
          }
       } else
       {
-        this->last_pulse = pdata->ts_value;
+        this->last_pulse = pdata.ts_value;
       }
-     delete pdata;
+     //delete pdata;
     } else
     {
     if (this->last_pulse != 0)
@@ -85,8 +85,8 @@ void IRAM_ATTR PulseMeterSensor::gpio_intr(PulseMeterSensor *sensor) {
 
   if ((now - sensor->last_irq)/1000 > 100) // 100mSec lockout
    { 
-     auto *pdata = new pulse_data_t;
-     pdata->ts_value = now;
+     pulse_data_t pdata;// = new pulse_data_t;
+     pdata.ts_value = now;
      xQueueSendFromISR(sensor->pulse_q_, &pdata, NULL);
    }
    
